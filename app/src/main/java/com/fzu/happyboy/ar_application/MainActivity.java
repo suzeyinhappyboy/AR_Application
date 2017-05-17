f21204a9e2b6706e524f47d8603ca9cf514a58ef
suzeyinhappyboy <619388013@qq.com>
2017-05-17 16:40:08 +0800

    Initial commit

---
+++ app/src/main/java/com/fzu/happyboy/ar_application/MainActivity.java
@@ -0,0 +1,30 @@
+package com.fzu.happyboy.ar_application;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "12";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.news_articles);
+
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+    
+}
